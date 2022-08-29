c7a381be9ef6c1d58c77aafbbb9a17ba3cccda84
Mucahid Canbey <mucahid.canbey@gmail.com>
2022-08-29 23:04:01 +0300

    shop bölümü düzenlendi

--- lib/pages/shop.dart
+++ lib/pages/shop.dart
@@ -1,12 +1,67 @@
 import 'package:flutter/material.dart';
 
-class UserShop extends StatelessWidget {
-  const UserShop({super.key});
+import '../util/shop_grid.dart';
+
+class UserShop extends StatelessWidget with _ColorsProje {
+  UserShop({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Shop')),
-    );
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: colorTrans,
+          title: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Shop',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                Row(
+                  children: const [
+                    Icon(Icons.calendar_today),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Icon(Icons.menu)
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(4),
+                  child: Container(
+                    padding: const EdgeInsets.all(4.0),
+                    color: Colors.grey.shade300,
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.search,
+                          color: Colors.grey.shade500,
+                        ),
+                        Text(
+                          'Search',
+                          style: TextStyle(color: Colors.grey.shade500, fontSize: 12),
+                        )
+                      ],
+                    ),
+                  )),
+            ),
+            Expanded(child: ShopGrid())
+          ],
+        ));
   }
+}
+
+class _ColorsProje {
+  Color colorTrans = Colors.transparent;
 }
