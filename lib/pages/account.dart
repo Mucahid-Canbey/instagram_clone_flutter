2a13e2a00351e9767db7e20fed3ed7ee8ded5b33
Mucahid Canbey <mucahid.canbey@gmail.com>
2022-08-31 23:46:11 +0300

    account bölümü düzenlendi

--- lib/pages/account.dart
+++ lib/pages/account.dart
@@ -1,12 +1,159 @@
 import 'package:flutter/material.dart';
+import 'package:instagram/util/account_tab_1.dart';
+import 'package:instagram/util/account_tab_2.dart';
+import 'package:instagram/util/account_tab_3.dart';
+import 'package:instagram/util/account_tab_4.dart';
+import 'package:instagram/util/buble_stories.dart';
 
 class UserAccount extends StatelessWidget {
   const UserAccount({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('Account')),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        body: Column(
+          children: [
+            // Profil Bölümü
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, top: 60.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(color: Colors.grey.shade300, shape: BoxShape.circle),
+                  ),
+                  //Gönderi ve Takipçi Bölümü
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              '11',
+                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                            ),
+                            Text('Post')
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              '888',
+                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                            ),
+                            Text('Fllowers')
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              '1111',
+                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                            ),
+                            Text('Fllowing'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // Name and bio
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('naruto.mu', style: TextStyle(fontWeight: FontWeight.bold)),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 2.0),
+                        child: Text('music & game'),
+                      ),
+                      Text(
+                        'twitter.com/UstaSensei',
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: GestureDetector(
+                      onTap: (){
+                        
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(5.0),
+                        child: Center(child: Text('Edit Profil')),
+                        decoration: BoxDecoration(border: Border.all(color: Colors.grey), borderRadius: BorderRadius.circular(5)),
+                      ),
+                    ),
+                  )),
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: Container(
+                      padding: EdgeInsets.all(5.0),
+                      child: Center(child: Text('Ad Tools')),
+                      decoration: BoxDecoration(border: Border.all(color: Colors.grey), borderRadius: BorderRadius.circular(5)),
+                    ),
+                  )),
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: Container(
+                      padding: EdgeInsets.all(5.0),
+                      child: Center(child: Text('Insights')),
+                      decoration: BoxDecoration(border: Border.all(color: Colors.grey), borderRadius: BorderRadius.circular(5)),
+                    ),
+                  )),
+                ],
+              ),
+            ),
+
+            //Stories
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+              child: Row(
+                children: [
+                  BubbleStories(text: 'Story 1'),
+                  BubbleStories(text: 'Story 2'),
+                  BubbleStories(text: 'Story 3'),
+                  BubbleStories(text: 'Story 4'),
+                ],
+              ),
+            ),
+            const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.grid_3x3_outlined)),
+                Tab(icon: Icon(Icons.video_call)),
+                Tab(icon: Icon(Icons.shop)),
+                Tab(icon: Icon(Icons.person)),
+              ],
+            ),
+            Expanded(
+                child: TabBarView(
+              children: [AccountTab1(), AccountTab2(), AccountTab3(), AccountTab4()],
+            ))
+          ],
+        ),
+      ),
     );
   }
 }
